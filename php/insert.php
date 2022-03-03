d693c643e312299d0bc40e81694df5912b7d3c20
gandhamathan <gandhamathanv.20cse@kongu.edu>
2022-03-03 21:56:47 +0530

    bug find

---
+++ php/insert.php
@@ -0,0 +1,16 @@
+<?php
+include 'connection.php';
+$Schoolname=$_POST['Schoolname'];
+$Schoolid=$_POST['Schoolid '];
+$Year=$_POST['Year '];
+$Area=$_POST['Area '];
+$rooms=$_POST['rooms '];
+$Labs=$_POST['Labs '];
+$Capacity=$_POST['Capacity '];
+$Email=$_POST['Email '];
+$Phone=$_POST['Phone '];
+$state=$_POST['state '];
+$gender=$_POST['gender '];
+$whats=$_POST['whats '];
+$email=$_POST['email '];
+echo $Schoolname;
